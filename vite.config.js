06be662794dc4edaed9674fb53d7a507e761031b
NqobileNgobeni <115787995+NqobileNgobeni@users.noreply.github.com>
2024-05-28 14:10:16 +0200

    first

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/ntsaki_protection/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
